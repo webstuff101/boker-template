ddc43c143cffc304a2931bbc90d9524ec30666a1
Vibe Nuage Agent <vibe@mistral.ai>
2026-09-25 19:35:15 +0000

    Update error.php for Joomla 6 compatibility
    - Added Text namespace import
    - Replaced hardcoded text with language strings:
      - '404' -> $this->error->getCode()
      - 'Page not found' -> Text::_('JERROR_PAGE_NOT_FOUND')
      - Static text -> Text::_('JERROR_LAYOUT_PAGE_NOT_FOUND')
      - 'go back.' -> Text::_('JERROR_LAYOUT_GO_BACK')
    - Added IE9 conditional comment with html5.js polyfill
    
    Co-authored-by: webstuff101 <webstuff101@users.noreply.github.com>

--- templates/boker/error.php
+++ templates/boker/error.php
@@ -13,6 +13,7 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Language\Text;
 
 // Get language and direction
 $doc = Factory::getDocument();
@@ -25,19 +26,20 @@ $doc = Factory::getDocument();
 	<title><?php echo $this->error->getCode(); ?> - <?php echo $this->title; ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php echo Uri::base(); ?>templates/<?php echo $this->template; ?>/css/style.css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!--[if lt IE 9]>
+		<script src="<?php echo Uri::base(); ?>media/jui/js/html5.js"></script>
+	<![endif]-->
 </head>
 <body class="error">
 	<center>
 		<div class="errorbox">
 			<div class="block">
-				<h1>404</h1>
-				<h3>Page not found</h3>
+				<h1><?php echo $this->error->getCode(); ?></h1>
+				<h3><?php echo Text::_('JERROR_PAGE_NOT_FOUND'); ?></h3>
 			</div>
 			<p>
-				Sorry! The page you are looking for cannot be found. Please use 
-				the provided search box to find what you are looking for, 
-				click on our top navigational menu, or 
-				<a onclick="window.history.back()">go back.</a>
+				<?php echo Text::_('JERROR_LAYOUT_PAGE_NOT_FOUND'); ?>
+				<a onclick="window.history.back()"><?php echo Text::_('JERROR_LAYOUT_GO_BACK'); ?></a>
 			</p>
 		</div>
 	</center>
